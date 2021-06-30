3410009566beb764033245d7380a611fce035879
钟小白Core <Core2002@163.com>
2021-07-01 02:00:55 +0800

    add license for source code

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ *  Copyright (c) 2021 NekokeCore
+ *  QQ-Image-Cut is licensed under Mulan PSL v2.
+ *  You can use this software according to the terms and conditions of the Mulan PSL v2.
+ *  You may obtain a copy of Mulan PSL v2 at:
+ *           http://license.coscl.org.cn/MulanPSL2
+ *  THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *  See the Mulan PSL v2 for more details.
+ */
+
 fs = require('fs');
 gm = require('gm')
 function fuckErr(err) { if (err) console.log('日志：' + err) }
--- 点我运行.cmd
+++ 点我运行.cmd
@@ -1,4 +1,13 @@
 :: https://gitee.com/nekokecore/QQ-Image-Cut
+:: 
+::  Copyright (c) 2021 NekokeCore
+::  QQ-Image-Cut is licensed under Mulan PSL v2.
+::  You can use this software according to the terms and conditions of the Mulan PSL v2.
+::  You may obtain a copy of Mulan PSL v2 at:
+::           http://license.coscl.org.cn/MulanPSL2
+::  THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+::  See the Mulan PSL v2 for more details.
+:: 
 @echo off
 set path=GraphicsMagick-1.3.36-Q16;node-v14.17.1-win-x64;%path%
 :run
